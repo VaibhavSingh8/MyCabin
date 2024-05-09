33c799e1df3273a89ab1cd83c497590c021ef255
Vaibhav Singh <vaibhavsingh@Vaibhavs-Laptop.local>
2024-05-09 12:02:50 +0530

    Header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <header className="bg-indigo-500 py-6 px-6 text-indigo-50 font-poppins font-medium rounded-b-xl">
-      Header
+    <header className="bg-indigo-500 py-6 px-6 text-indigo-50 font-poppins font-medium rounded-b-xl text-center">
+      Book My Cabin
     </header>
   );
 };
