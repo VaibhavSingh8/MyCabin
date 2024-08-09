5c58c5dbd34a67bc7629629d3a0f6cdb2a78fe63
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-08-09 12:30:29 +0530

    fix: Workaround for navigate not working on first login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,17 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
-  const navigate = useNavigate();
+  // const queryClient = useQueryClient();
+  //const navigate = useNavigate();
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi(email, password),
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       if (data.isAuthenticated) {
-        console.log("Attempting to navigate to dashboard");
         toast.success("Logged in successfully");
-        navigate("/dashboard");
+        console.log("navigating...");
+        setTimeout(() => {
+          window.location.href = "/dashboard";
+        }, 1000);
       } else {
         toast.error(data.error || "An error occurred during login");
       }
