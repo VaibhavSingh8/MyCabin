6877dd9dd6e29aa10e30b292d9f735130109294b
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-08-09 22:54:04 +0530

    fix: Navigation on first login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,29 +1,34 @@
 import { useMutation } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 export function useLogin() {
+  const [auth, setAuth] = useState(false);
   // const queryClient = useQueryClient();
-  //const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi(email, password),
-    onSuccess: async (data) => {
+    onSuccess: (data) => {
       if (data.isAuthenticated) {
+        setAuth(true);
         toast.success("Logged in successfully");
-        console.log("navigating...");
-        setTimeout(() => {
-          window.location.href = "/dashboard";
-        }, 1000);
       } else {
-        toast.error(data.error || "An error occurred during login");
+        toast.error(data.error || "Authentication");
       }
     },
     onError: (err) => {
       toast.error(err.message || "An error occurred during login");
     },
   });
+
+  useEffect(() => {
+    if (auth) {
+      navigate("/dashboard");
+    }
+  }, [auth, navigate]);
 
   return { login, isLoading };
 }
